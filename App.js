f4dcd539a0d0d68e06f4175697f810b2dab3d6fc
unknown <alexander.ivantsov.hw@gmail.com>
2023-01-03 17:49:05 +0300

    added new file App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/Header/Header";
+import BodyCont from "./components/BodyContainer/BodyCont.jsx";
+import Wrapper from './style.module.css';
+import Foot from './components/Foot/Foot.jsx';
+const App = () => {
+  return (
+    <div className={Wrapper.wrapper}>
+      <Header />
+      <BodyCont />
+      <Foot />
+    </div>
+  );
+}
+
+export default App;
